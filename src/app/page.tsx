174631f9cf59db1f37e278b02fc47dc95afd8e5b
clockcode <cagdasmuldur@gmail.com>
2025-04-03 00:47:36 -0400

    fix: build lint bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import HeroSection from '@/components/HeroSection';
 import ServicesSection from '@/components/ServicesSection';
 import BenefitsSection from '@/components/BenefitsSection';
-import ContactSection from '@/components/ContactSection';
 import Footer from '@/components/Footer';
 
 export default function Home() {
